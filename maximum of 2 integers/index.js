d6b44934ccf3a61908d5b6d0861455f1def08167
abdelrouf-ali <84926894+abdelrouf-ali@users.noreply.github.com>
2023-03-05 12:54:58 +0200

    Add files via upload

---
+++ maximum of 2 integers/index.js
@@ -0,0 +1,20 @@
+
+
+var firstnum = prompt("Enter first number");
+var secondtnum = prompt("Enter second number");
+
+
+
+if (firstnum > secondtnum) {
+
+    alert("the maximum = " + firstnum);
+}
+
+
+else {
+
+    alert("the maximum = " + secondtnum);
+}
+
+
+
